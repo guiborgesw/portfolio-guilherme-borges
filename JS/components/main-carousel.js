c8ee9a5206097c1824a51f245ae54f28b4722676
Guilherme Borges <borgesg226@gmail.com>
2025-12-01 16:03:43 -0300

    Add my LinkedIn to footer

--- JS/components/main-carousel.js
+++ JS/components/main-carousel.js
@@ -1,15 +1,3 @@
-// 1 - Acessar a Janela (Browser)
-// 2 - Pegar HTML todo
-// 3 - Pegar botão
-// 4 - Saber que está sendo clicado no botão
-
-// 5 - Acessar a Janela (Browser)
-// 6 - Pegar HTML todo
-// 7 - Pegar o elements
-// 8 - Mover o elements para direita
-// 9 - 
-
-
 const btnRight = document.querySelector('.button-arrow.-right');
 const btnLeft = document.querySelector('.button-arrow.-left');
 const elements = document.querySelector('.elements');
