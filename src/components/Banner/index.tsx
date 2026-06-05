ecfd3cdaf2e2e2edc1226f06a4cc3f1d4b8e074c
Guilherme Borges <borgesg226@gmail.com>
2026-06-05 19:38:46 -0300

    Add Banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,23 @@
+import * as S from './Banner.styles'
+
+export function Banner() {
+  return (
+    <S.Section>
+      <S.Wrapper>
+        <S.Box>
+          <S.Title>Guilherme Borges</S.Title>
+          <S.Subtitle>Software Developer</S.Subtitle>
+        </S.Box>
+        <S.Navigation>
+          <S.Button
+            href="/src/assets/img/Guilherme-Borges-CV.pdf"
+            download="Guilherme-Borges-CV.pdf"
+          >
+            Download CV
+          </S.Button>
+          <S.Button href="#contatos">Contatos</S.Button>
+        </S.Navigation>
+      </S.Wrapper>
+    </S.Section>
+  )
+}
