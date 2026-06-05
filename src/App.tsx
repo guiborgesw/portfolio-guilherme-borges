4b0b25aff7e45cc4baafd7eedc8ca5f66d82728e
Guilherme Borges <borgesg226@gmail.com>
2026-06-05 20:13:17 -0300

    Feat: add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { Banner } from './components/Banner'
-import { Navbar } from './components/Navbar'
-import { Projects } from './components/Projects'
-import { Skills } from './components/Skills'
-import { About } from './components/About'
+import { Navbar }    from './components/Navbar'
+import { Banner }    from './components/Banner'
+import { Skills }    from './components/Skills'
+import { Projects }  from './components/Projects'
+import { About }     from './components/About'
+import { Contacts }  from './components/Contacts'
+import { Footer }    from './components/Footer'
 
 export function App() {
   return (
@@ -12,6 +14,8 @@ export function App() {
       <Skills />
       <Projects />
       <About />
+      <Contacts />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import linkedinIcon from '../../assets/img/ico_linkedin.png'
+import * as S from './Footer.styles'
+
+export function Footer() {
+  return (
+    <S.FooterWrapper>
+      <S.FooterLink
+        href="https://www.linkedin.com/in/guiborgesw/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <S.FooterIcon src={linkedinIcon} alt="Logo do LinkedIn em pixel art" />
+      </S.FooterLink>
+    </S.FooterWrapper>
+  )
+}
