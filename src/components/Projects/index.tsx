44705c446b4474649342357698f84be8fe95c7cd
Guilherme Borges <borgesg226@gmail.com>
2026-09-08 21:31:11 -0300

    Refactor: improvment on imports of Projects page

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,32 +1,45 @@
 import { projects } from '../../data/projects'
-import * as S from './Projects.styles'
+import { 
+  Button, 
+  Card, 
+  CardButtons, 
+  CardDescription, 
+  CardInfo, 
+  CardThumb, 
+  CardTitle, 
+  Section, 
+  StackList, 
+  StackTag, 
+  Title, 
+  WrapperCard,
+} from './Projects.styles'
 
 export function Projects() {
   return (
-    <S.Section id="projetos">
-      <S.Title>Projetos</S.Title>
-      <S.WrapperCard>
+    <Section id="projetos">
+      <Title>Projetos</Title>
+      <WrapperCard>
         {projects.map((project) => (
-          <S.Card key={project.name}>
-            <S.CardThumb $bg={project.thumbBg}>
+          <Card key={project.name}>
+            <CardThumb $bg={project.thumbBg}>
               <img src={project.image} alt={project.alt} />
-            </S.CardThumb>
-            <S.CardInfo>
-              <S.CardTitle>{project.name}</S.CardTitle>
-              <S.CardDescription>{project.description}</S.CardDescription>
-              <S.StackList>
+            </CardThumb>
+            <CardInfo>
+              <CardTitle>{project.name}</CardTitle>
+              <CardDescription>{project.description}</CardDescription>
+              <StackList>
                 {project.stack.map((tech) => (
-                  <S.StackTag key={tech}>{tech}</S.StackTag>
+                  <StackTag key={tech}>{tech}</StackTag>
                 ))}
-              </S.StackList>
-              <S.CardButtons>
-                <S.Button href={project.demoUrl} target="_blank" $variant="demo">▶ Demo</S.Button>
-                <S.Button href={project.githubUrl} target="_blank" $variant="github">GitHub</S.Button>
-              </S.CardButtons>
-            </S.CardInfo>
-          </S.Card>
+              </StackList>
+              <CardButtons>
+                <Button href={project.demoUrl} target="_blank" $variant="demo">▶ Demo</Button>
+                <Button href={project.githubUrl} target="_blank" $variant="github">GitHub</Button>
+              </CardButtons>
+            </CardInfo>
+          </Card>
         ))}
-      </S.WrapperCard>
-    </S.Section>
+      </WrapperCard>
+    </Section>
   )
 }
