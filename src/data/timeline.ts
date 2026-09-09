6c957548fcd7af445e2a9eb58d2febc5e131f197
Guilherme Borges <borgesg226@gmail.com>
2026-09-09 16:48:43 -0300

    Chore: add new logos to companys on section About

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -5,8 +5,8 @@ export type TimelineItem = {
 }
 
 export const timeline: TimelineItem[] = [
-  { year: '2023', logo: '/src/assets/Luft-logo.png', alt: 'Logo Luft Solutions' },
-  { year: '2024', logo: '/src/assets/LOGO-CAMP.png', alt: 'Logo CAMP Pinheiros' },
-  { year: '2025', logo: '/src/assets/GRSA-logo.png', alt: 'Logo GRSA Compass' },
-  { year: '2025-2026', logo: '/src/assets/logo-gps.png', alt: 'Logo GPS' },
+  { year: '2023', logo: '/src/assets/Logo-LuftLogistics.png', alt: 'Logo Luft Logistics' },
+  { year: '2024', logo: '/src/assets/Logo-Camp.png', alt: 'Logo CAMP Pinheiros' },
+  { year: '2024-2025', logo: '/src/assets/Logo-GRSA.png', alt: 'Logo GRSA' },
+  { year: '2025-2026', logo: '/src/assets/Logo-GrupoGPS.png', alt: 'Logo Grupo GPS' },
 ]
