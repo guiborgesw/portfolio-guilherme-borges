5ce3115c187c20cddc0c6d8da2da55dd440e9a05
Guilherme Borges <borgesg226@gmail.com>
2026-09-08 21:32:58 -0300

    Chore: add new colors on my main theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,18 @@
 export const theme = {
+    'blue-100': '#93C5FD',
+    'blue-200': '#8FB0FF',
     'blue-300': '#4680DB',
     'blue-400': '#3a6fc4',
     'blue-500': '#5679B0',
+    'blue-600': '#2563EB',
     'blue-700': '#3B4487',
     'blue-900': '#001366',
     'gray-100': '#AAAAAA',
+    'gray-200': '#9AA1B5',
     'gray-300': '#666666',
     'gray-500': '#555555',
     'gray-700': '#3A4042',
-    'black': '#000000',
+    'black': '#05060A',
     'white': '#FFFFFF',
 }
 
